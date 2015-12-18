c8855808ea5565cc54ee71531e49451548266546
bozidar <bozidar.labas@hotmail.com>
2015-12-18 13:44:31 +0100

    Initial Commit

---
+++ FeedbackApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedbackApp
+//
+//  Created by Bozidar on 18.12.2015..
+//  Copyright © 2015. Bozidar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
